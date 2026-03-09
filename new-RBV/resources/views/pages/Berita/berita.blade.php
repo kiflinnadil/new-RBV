6d64cd17afc9ddefb7c30b7d15aee7d94be74086
rakafahrul <155793828+rakafahrul@users.noreply.github.com>
2026-03-09 13:57:28 +0700

    update berita,artikel,video

--- new-RBV/resources/views/pages/Berita/berita.blade.php
+++ new-RBV/resources/views/pages/Berita/berita.blade.php
@@ -37,21 +37,30 @@
         </div>        
 
 
+        <div class="max-w-[1440px] mx-auto px-10 py-10"> <div class="grid grid-cols-1 md:grid-cols-3 gap-12 px-2"> @forelse ($videoberita as $video)
+            <div class="bg-white rounded-[40px] shadow-[0_15px_40px_rgba(0,0,0,0.08)] overflow-hidden flex flex-col transition-all duration-300 hover:-translate-y-2 hover:shadow-[0_25px_60px_rgba(0,0,0,0.15)]">
+                
+                <div class="relative w-full aspect-square overflow-hidden bg-gray-100">
+                    <div class="absolute top-5 right-5 z-10 flex flex-col gap-3">
+                        <a href="#" class="p-2.5 bg-[#00A14C] text-white rounded-xl shadow-lg hover:scale-110 transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                            </svg>
+                        </a>
+                        <form action="{{ route('berita.destroy', $video->id) }}" method="POST" onsubmit="return confirm('Hapus berita ini?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="p-2 bg-[#E11D48] text-white rounded-lg shadow-md hover:scale-110 transition">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </form>
+                    </div>
 
-        <div class="max-full mx-auto px-16 py-10">
-
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
-                @forelse ($videoberita as $video)
-                    <div class="bg-white rounded-2xl shadow-lg hover:shadow-2xl transition duration-300 overflow-hidden flex flex-col">
-                        
-                        <div class="aspect-video w-full">
-                            <iframe src="{{ $video->link }}"
-                                    class="w-full h-full"
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen>
-                            </iframe>
-                        </div>
+                    <div class="aspect-[4/3] w-full bg-gray-200 overflow-hidden">
+                        <img src="{{ asset('storage/' . $video->thumbnail) }}" class="w-full h-full object-cover" alt="{{ $video->judul }}">
+                    </div>
 
                         <div class="p-5 flex flex-col flex-grow">
                             <p class="text-sm text-[#00A14c] font-semibold mb-1">
